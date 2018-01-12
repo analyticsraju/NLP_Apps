ec44f36306d82635c6e9c06aca3663b115f16157
Analytics.Raju@GMail.com <31151331+analyticsraju@users.noreply.github.com>
2018-01-12 15:28:03 +0530

    nlp basics

---
+++ 1.NLP_Exercise/sentance_tokenization.py
@@ -0,0 +1,11 @@
+#https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+from nltk.tokenize import sent_tokenize
+sentence = sent_tokenize("Hello NLP learner. This is an NLP python script")
+print (sentence)
+
+story1 = 'Hi, This is Raju. I am data scientist. '
+story2 = 'I love to work on machine learing.'
+story3 = 'I love to learn data science technologies'
+story1=story1+story2+story3
+story = sent_tokenize(story1) 
+print(story)
